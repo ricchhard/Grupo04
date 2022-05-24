057dd9fdc8975fa509b39ea1252493fd9a5c3baa
RichardCaraguay <59847272+ricchhard@users.noreply.github.com>
2022-05-24 18:29:30 -0500

    Add files via upload

---
+++ Taller3/html/validaremail.js
@@ -0,0 +1,23 @@
+﻿function validar() {
+	// body...
+	var correo, telefono, expresion;
+	correo= document.getElementById('txtEmail').value;
+	telefono= document.getElementById('txtTelefono').value;
+	expresion= /^[a-z][\w.-]+@\w[\w.-]+\.[\w.-]*[a-z][a-z]$/i;
+	
+	/*if(checkSubmit()){
+		alert("Script checkSubmit() ejecutado correctamente");
+	}
+	else{
+		alert("Script checkSubmit() No se ejecuto correctamente");
+	}*/
+
+	if(!expresion.test(correo)){
+		alert("El correo no es válido");
+		return false;
+	}
+	else{
+		alert("Correo válido");
+		return true;
+	}
+	
